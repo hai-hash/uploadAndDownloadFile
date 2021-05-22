cfd2bd4e46e2585373c9f6f7a1fc4cf4ab328b63
hai-hash <65753082+hai-hash@users.noreply.github.com>
2021-05-23 00:14:41 +0700

    Update HomeController.java

--- src/main/java/com/laptrinhweb/HomeController.java
+++ src/main/java/com/laptrinhweb/HomeController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +12,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 import com.laptrinhweb.domain.Image;
+import com.laptrinhweb.repository.ImageRepository;
 
 @Controller
 @RequestMapping(path = "/")
 public class HomeController {
+	@Autowired
+	private ImageRepository imageRepo;
 	
 	private RestTemplate rest = new RestTemplate();
 	@GetMapping
 	public String home(Model model) {
 		List<Image> images = new ArrayList<Image>();
-		images = Arrays.asList(rest.getForObject("https://file-managementt.herokuapp.com/api", Image[].class));
+//		images = Arrays.asList(rest.getForObject("https://file-managementt.herokuapp.com/api", Image[].class));
+		images = imageRepo.findAll();
 		model.addAttribute("images", images);
 		return "file";
 	}
