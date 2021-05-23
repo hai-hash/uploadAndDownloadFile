da0b47c07ac162c2403e1da20d4ab0714db5d134
hai-hash <65753082+hai-hash@users.noreply.github.com>
2021-05-23 07:07:07 +0700

    upload

--- src/main/java/com/laptrinhweb/HomeController.java
+++ src/main/java/com/laptrinhweb/HomeController.java
@@ -11,11 +11,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
+import com.laptrinhweb.domain.Image;
+import com.laptrinhweb.repository.ImageRepository;
+
 @Controller
 @RequestMapping(path = "/")
 public class HomeController {
+	@Autowired
+	private ImageRepository imageRepo;
 	@GetMapping
 	public String home(Model model) {
+		List<Image> images = new ArrayList<Image>();
+		images = imageRepo.findAll();
+		model.addAttribute("images", images);
 		return "file";
 	}
 	
