403bbadc22e4c0fffeeb42692643f4a79260e7bc
hai-hash <65753082+hai-hash@users.noreply.github.com>
2021-05-26 15:03:06 +0700

    upload

--- src/main/java/com/laptrinhweb/ProjectWebApplication.java
+++ src/main/java/com/laptrinhweb/ProjectWebApplication.java
@@ -4,15 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import com.laptrinhweb.domain.FileStorageProperties;
-
-
-
 @SpringBootApplication
-@EnableConfigurationProperties({
-    FileStorageProperties.class
-})
 public class ProjectWebApplication {
 
 	public static void main(String[] args) {
